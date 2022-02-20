20ef62a7cc6c71a517dd45ea7afbf080d2fe2ad9
vishal929 <48373882+vishal929@users.noreply.github.com>
2022-02-20 16:39:16 -0500

    Needed to move onClick into main activity, fragment logic is weird

--- app/src/main/java/com/example/cs527_rdsmysql/MainActivity.kt
+++ app/src/main/java/com/example/cs527_rdsmysql/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.cs527_rdsmysql
 
+import ConnectionUtility.RDSConnection
+import ConnectionUtility.ResultObject
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -19,6 +26,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // setting button onclick listener
+        val sqlButton: Button = findViewById<Button>(R.id.executeQueryButton)
+        sqlButton.setOnClickListener { view ->
+            queryButtonOnClick(view)
+        }
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -31,5 +44,27 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun queryButtonOnClick(view: View){
+        val sqlIn: EditText = findViewById(R.id.sqlInEditText)
+        // grabbing results based on sql text in the sqlInTextView
+        val jdbcUrl = "URL HERE"
+        val user = "USER HERE"
+        val pass = "PASS HERE"
+        val conn: RDSConnection = RDSConnection(jdbcUrl, user, pass)
+        // try a connection here
+        conn.connect()
+        // if successful, we move onto sql query
+        conn.executeSQL(sqlIn.text.toString())
+        // if we executed the query successfully, we move onto results gathering
+        // CHECK FOR NULL HERE
+        val results: ResultObject? = conn.grabData(conn.sqlResultSet!!)
+        // IF results is null, something went wrong with grabbing data
+        // if we reached here we successfully grabbed our data
+        val sqlOut : TextView = findViewById(R.id.sqlOutTextView)
+        val outString: String = results!!.intColumns.size.toString() + ", " + results.stringColumns.size.toString()
+        sqlOut.text = outString
+        Log.d("CUSTOM",outString)
     }
 }
--- app/src/main/java/com/example/cs527_rdsmysql/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cs527_rdsmysql/ui/home/HomeFragment.kt
@@ -3,17 +3,19 @@ package com.example.cs527_rdsmysql.ui.home
 import ConnectionUtility.RDSConnection
 import ConnectionUtility.ResultObject
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cs527_rdsmysql.R
 import com.example.cs527_rdsmysql.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(){
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -36,12 +38,6 @@ class HomeFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
-        // setting onClick listener for the
-        root.findViewById<Button>(R.id.executeQueryButton).setOnClickListener { view ->
-            if (view != null) {
-                queryButtonOnClick(view)
-            }
-        }
         return root
     }
 
@@ -50,21 +46,5 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    fun queryButtonOnClick(View:View){
-        val sqlIn:TextView = View.findViewById(R.id.sqlInTextView)
-       // grabbing results based on sql text in the sqlInTextView
-        val conn: RDSConnection = RDSConnection("jdbcURLhere", "userHere", "passHere")
-        // try a connection here
-        conn.connect()
-        // if successful, we move onto sql query
-        conn.executeSQL(sqlIn.text.toString())
-        // if we executed the query successfully, we move onto results gathering
-        // CHECK FOR NULL HERE
-        val results: ResultObject? = conn.grabData(conn.sqlResultSet!!)
-        // IF results is null, something went wrong with grabbing data
-        // if we reached here we successfully grabbed our data
-        val sqlOut : TextView = View.findViewById(R.id.sqlOutTextView)
-        val outString: String = results!!.intColumns.size.toString() + ", " + results.stringColumns.size.toString()
-        sqlOut.text = outString
-    }
+
 }
